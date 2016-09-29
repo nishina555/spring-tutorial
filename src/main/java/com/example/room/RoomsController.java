278c39420a382ed3d1c0b47df196c9942372500d
nishina <n.toshi.1.014@gmail.com>
2016-09-29 20:17:45 +0900

    complete rooms list function

--- src/main/java/com/example/room/RoomsController.java
+++ src/main/java/com/example/room/RoomsController.java
@@ -2,21 +2,23 @@ package com.example.room;
 
 import com.domain.model.ReservableRoom;
 import com.domain.service.room.RoomService;
-import com.sun.tools.javac.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by nishina on 2016/09/28.
  */
 @Controller
 @RequestMapping("rooms")
-public class RoomController {
+public class RoomsController {
     @Autowired
     RoomService roomService;
 
@@ -26,6 +28,13 @@ public class RoomController {
         List<ReservableRoom> rooms = roomService.findReservableRooms(today);
         model.addAttribute("date", today);
         model.addAttribute("rooms", rooms);
-        return "model/listRooms";
+        return "room/listRooms";
+    }
+
+    @GetMapping(path = "{date}")
+    String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date, Model model) {
+        List<ReservableRoom> rooms = roomService.findReservableRooms(date);
+        model.addAttribute("rooms", rooms);
+        return "room/listRooms";
     }
 }
